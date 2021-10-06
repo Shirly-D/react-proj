5bc855b82d17a0e868376d249d4dfbcc9bbf1a8f
Shirly <shirlyd@axioned.com>
2021-10-06 20:06:10 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,18 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import FunctionComp from './Components/FunctionComp';
+import {ClassComp, ClassComp1} from './Components/ClassComp';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Hello world!!</h1>
+      <h2>Good day</h2>
+      <FunctionComp></FunctionComp>
+      <ClassComp></ClassComp>
+      <ClassComp1></ClassComp1>
     </div>
+    
   );
 }
 
---
+++ src/Components/FunctionComp.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function FunctionComp() {
+    return <p>This is function component.</p>
+}
+
+export default FunctionComp;
