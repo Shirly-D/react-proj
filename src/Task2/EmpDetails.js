21b323f3bf2dbc2e5f0203d468612f39453a2df4
Shirly <shirlyd@axioned.com>
2021-10-13 19:08:54 +0530

    pass data through params final commit

--- src/Task2/EmpDetails.js
+++ src/Task2/EmpDetails.js
@@ -1,12 +1,15 @@
 // import React from 'react';
 import empData from '../Util/taskData.js';
-import Emp from './Employee.js';
 
 function EmployeeDetails(props) {
     const displayData = empData.find(data => data.id === props.match.params.id)
-    console.log(displayData)
+    console.log(displayData) 
 
-    return <Emp data = {displayData}></Emp>  
+    return <div className={displayData.className}>
+        <p>Name: {displayData.ename}</p>
+        <p>Joining: {displayData.joining}</p>
+        <p>Progress: {displayData.progress}%</p>
+    </div>
 }
 
 
